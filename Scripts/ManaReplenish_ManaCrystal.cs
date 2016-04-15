f017a51941958cc1ef6abb8481e5ec2bd17654f8
Kamzoki <kaittah@ymail.com>
2016-04-16 01:27:01 +0200

    Mana behavior done

--- Scripts/ManaReplenish_ManaCrystal.cs
+++ Scripts/ManaReplenish_ManaCrystal.cs
@@ -3,19 +3,41 @@ using System.Collections;
 
 public class ManaReplenish_ManaCrystal : MonoBehaviour
 {
-
+	//public:
+	// mana amount for each diamond
 	public float fl_manaAmount;
+	//Movement speed of each diamond
+	public float fl_movementSpeed;
+	//private:
+	// Player hp controller refrecne
 	private HPController_General HPC_GameObjectRef;
+	//controlling diamond behavior
+	private bool canChase = false;
+	//Refrence for player transform
+	private Vector3 vec_Player;
 
 	void Start ()
 	{
 		HPC_GameObjectRef = GameObject.FindWithTag ("Player").GetComponent <HPController_General> ();
 	}
 
+	void Update ()
+	{
+		
+		if (canChase == true) { // Traslate the diamond to the player
+			vec_Player = GameObject.FindWithTag ("Player").transform.position;
+			transform.position = Vector3.MoveTowards (transform.position, vec_Player, fl_movementSpeed * Time.deltaTime);
+		}
+	}
+
 	void OnCollisionEnter (Collision col)
 	{
-		if (col.gameObject.tag == "Player") {
-            
+		if (col.gameObject.tag == "Terrain") {// if the diamond hits a terrain, enable it's chasing behavior
+			canChase = true;
+		}
+
+		if (col.gameObject.tag == "Player") {// on collision with the player give more mana and modify mana power accordingly.
+
 			if (Mana.mana <= Mana.maxMana) {
 				Mana.mana += fl_manaAmount;
 				HPC_GameObjectRef.fl_tmpManabar += fl_manaAmount / Mana.maxMana;
@@ -24,7 +46,8 @@ public class ManaReplenish_ManaCrystal : MonoBehaviour
 
 			}
 			GameObject.Destroy (gameObject);
-            
+
 		}
+
 	}
 }
