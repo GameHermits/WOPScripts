f47c922666c67b8c790b4933fa560b690c8895ce
MAX ELDESOKY <MAX ELDESOKY>
2016-04-17 20:32:12 +0200

    the inventory code with functions done and showed on screen

--- Scripts/Inventory.cs
+++ Scripts/Inventory.cs
@@ -1,8 +1,86 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Image = UnityEngine.UI.Image;
+public class Inventory : MonoBehaviour{
 
-public static class Inventory {
+	public  Sprite empty_Sprite;
+	public  Image[] bag;
+	private int Ibag = 0;
+	private int IMAXbag = 6;
 
-	public static GameObject[] bag = new GameObject[6];
-	public static int Ibag = 0;
+	private static Inventory inv ;
+	public static Inventory IN {
+		get {
+			if(inv == null)
+				inv = Inventory.FindObjectOfType<Inventory>();
+			return Inventory.inv;
+		}
+	}
+
+	public void AddItem (Sprite item){
+		if (Ibag <= IMAXbag && bag [Ibag].sprite == empty_Sprite) {
+			bag [Ibag].sprite = item;
+			Ibag++;
+
+		} else {
+			for (int i = 0; i < IMAXbag; i++) {
+				if (bag[i].sprite == empty_Sprite) {
+					bag [i].sprite = item;
+					Ibag++;
+					break;
+				}
+			}
+		}
+
+	}
+
+	public bool CanUseItem (Sprite item){
+		for (int i = 0; i < bag.Length; i++) {
+			if (item == bag[i].sprite) {
+				bag [i].sprite = empty_Sprite;
+				Ibag--;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void RemoveItemNum (int index){
+		if (index <= IMAXbag) {
+			bag [index].sprite = empty_Sprite;
+			Ibag--;
+		}
+	}
+
+	public void RemoveItem (Sprite icon){
+		for (int i = 0; i < IMAXbag; i++) {
+			if (bag[i].sprite == icon) {
+				bag [i].sprite = empty_Sprite;
+				Ibag--;
+				break;
+			}
+		}
+	}
+		
+	public void RemoveLastItem(){
+		if (bag[Ibag].sprite != empty_Sprite) {
+			bag [Ibag].sprite = empty_Sprite;
+			Ibag--;
+		} else {
+			for (int i = IMAXbag-1; i >= 0; i--) {
+				if (bag[i].sprite != empty_Sprite) {
+					bag [i].sprite = empty_Sprite;
+					Ibag--;
+					break;
+				}
+			}
+		}
+	}
+
+	public void RemoveAllItem (){
+		for (int i = 0; i < bag.Length; i++) {
+			bag [i].sprite = empty_Sprite;
+		}
+		Ibag = 0;
+	}
 }
--- Scripts/ItemCollect_Item.cs
+++ Scripts/ItemCollect_Item.cs
@@ -1,17 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using Image = UnityEngine.UI.Image;
 public class ItemCollect_Item : MonoBehaviour {
 
-	public static bool isUseable = false; // to check if the player can collect items
-
+	//public static bool isUseable = false; // to check if the player can collect items
+	public Sprite icon;
 	void OnTriggerEnter ( Collider col){ // when the item collition to the player but it in the inventory
 
 		if (col.gameObject.tag == "Player") {
-			Inventory.bag[Inventory.Ibag] = gameObject;
-			Inventory.Ibag++;
-			isUseable = true;
-			gameObject.SetActive(false);
+			Inventory.IN.AddItem (icon);
+			Destroy (this.gameObject);
+			//isUseable = true;
+			//gameObject.SetActive(false);
 		}
 	}
 }
