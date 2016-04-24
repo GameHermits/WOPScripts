f03bf9a3f7f6b10d0a21df83834854a600c5b514
Kamzoki <kaittah@ymail.com>
2016-04-24 20:16:44 +0200

    not yet

--- Scripts/CharacterSound_General.cs
+++ Scripts/CharacterSound_General.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class CharacterSound_General : MonoBehaviour
 {
+	//public:
+	public AudioClip[] Agony;
+	public AudioClip[] Death;
+	public AudioClip[] Attack;
 
-
+	//private:
+	private AudioSource Sounds;
 	// Use this for initialization
 	void Start ()
 	{
-	
+		Sounds = gameObject.GetComponent <AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		
 	}
 }
