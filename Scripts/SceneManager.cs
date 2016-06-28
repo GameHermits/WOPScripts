2bd2aca259b186b95ff12f780b2e0247a80e3e3f
Kamzoki <kaittah@ymail.com>
2016-06-28 20:43:05 +0200

    Updated

--- Scripts/SceneManager.cs
+++ Scripts/SceneManager.cs
@@ -14,40 +14,22 @@ public class SceneManager : MonoBehaviour
 	public static SceneManager SM;
 
 	//Public:
-	//Contain all Spawners empty gameObject in the current Scene.
-	public GameObject[] Spawners;
-	//Contain all LossEnemies in the Scene.
-	public GameObject[] LossEnemies;
 	//Contain all Checkpoints location in the Scene. Initially empty gameObject if there is no model avaliable. zero index in any level is always the starter location of the player
-	public Transform[] CheckPoints;
+	public CheckPointInfo[] CheckPoints;
 	//Contain all objectives in the current Scene.
 	public Objective[] Objectives;
 	//Indecate how many treasures are there in the Scene, each time the player opens one, this number is decreased
 	public int TreasureNumber;
 	//Indecates the over all enemy levels in the current Scene.
 	public int EnemiesLevel;
-
+	//This Index CheckPoints array.. and is modified by checkpoint objects, that is, whenever a player reach the next checkpoint, this index is increased by one.
+	public int checkpointIndex = 0;
 	//Private:
-	// The previous checkpoint index to Spawners array.
-	private int preCheckPoint_Spawners = 0;
-	//The current checkpoint index to Spawners array.
-	private int currentCheckPoint_Spawners = 0;
-	// The previous checkpoint index to LossEnemies array.
-	private int preCheckPoint_LossEnemies = 0;
-	//The current checkpoint index to LossEnemies array.
-	private int currentCheckPoint_LossEnemies = 0;
-	//The current checkpoint index to CheckPoints array.
-	private int currentCheckPoint_CheckPoints = 0;
-	/*Checkpoints Definition : Each array got two indexes, one that points for the latest checkpoint, and one that points to the current state. currectCheckPoint will increase by one everyTime a lossEnemy die
-	 * or a Spawner is destroied, so that it alwasy points to the player progress through the Scene in terms of spawners and loss enemies. the preCheckpoint will equals the currentCheckPoint whenever a player
-	 * saved the game in some checkpoint area in the Scene, so whenever the player dies, currentCheckpoint will equals preCheckpoint, and a function that spawn all object from the currentCheckPoint to the end
-	 * of the arraies will be called to reset the state of the Scene, while player location will teleport back to the checkpoint location.
-	 * */
 
 	//ObjectiveIndex should decrease everytime an objective is completed
 	private int ObjectiveIndex;
-	//This int indecate the total progress the player did in the level, it's calculated as the following Formula
-	//(100 - TreasureNumber - LossEnemies.Length - Spawners.Lenght - ObjectivesIndex)
+	/*This int indecate the total progress the player did in the level, it's calculated as the following Formula
+	(100 - TreasureNumber - LossEnemies.Length - Spawners.Lenght - ObjectivesIndex)*/
 	private int totalProgress = 0;
 
 
