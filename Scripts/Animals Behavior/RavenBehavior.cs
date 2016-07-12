b3c22b77a00656dc07c6f2c8765de40f5cc3c8d0
Kamzoki <kaittah@ymail.com>
2016-07-12 15:53:44 +0300

    bug fixed - RavenBehavior

--- Scripts/Animals Behavior/RavenBehavior.cs
+++ Scripts/Animals Behavior/RavenBehavior.cs
@@ -6,15 +6,22 @@ public class RavenBehavior : MonoBehaviour
 
 	//Private:
 	private GameObject go_PlayerLocationRef;
+	// Refrence to player gameobject
 	private float fl_DangerRange;
+	//distance in which raven start flying away
 	private Animator RavenAnimator;
+	//animator controller attached to gameObject
 	private int WayPointsIndex = 0;
+	//WayPoints Index
 	private bool canMove = false;
+	//Control the behavior of the raven
 	//Public:
 	public GameObject[] WayPoints;
+	// contain all the way points as empty gameobjects. It should be set manually from Inspector.
 	// Use this for initialization
 	void Awake ()
 	{
+		//Initialaization
 		RavenAnimator = gameObject.GetComponent <Animator> ();
 		go_PlayerLocationRef = GameObject.FindGameObjectWithTag ("Player");
 	}
@@ -22,6 +29,7 @@ public class RavenBehavior : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		//If Player is too close
 		fl_DangerRange = Vector3.Distance (gameObject.transform.position, go_PlayerLocationRef.transform.position);
 
 		if (fl_DangerRange <= 4) {
@@ -30,15 +38,22 @@ public class RavenBehavior : MonoBehaviour
 		Move ();
 	}
 
+	IEnumerator WaitforSeconds ()
+	{
+		yield return new WaitForSeconds (1f);
+		transform.position = Vector3.MoveTowards (gameObject.transform.position, WayPoints [WayPointsIndex].transform.position, 4 * Time.deltaTime);
+	}
+
 	private void Move ()
 	{
+		//Test canMove and determine weather raven 
 		if (canMove == true) {
 
 			if (WayPoints != null) {
 
 				if (Vector3.Distance (gameObject.transform.position, WayPoints [WayPointsIndex].transform.position) <= 0) {
 					canMove = false;
-					RavenAnimator.SetBool ("ShouldFly", false);
+					RavenAnimator.SetBool ("ShouldFly", false); //Used to send a message to animator controller to controll it's boolean parameters
 
 					if (WayPointsIndex != WayPoints.Length - 1)
 						WayPointsIndex++;
@@ -47,7 +62,7 @@ public class RavenBehavior : MonoBehaviour
 				} else {
 					transform.LookAt (WayPoints [WayPointsIndex].transform);
 					RavenAnimator.SetBool ("ShouldFly", true);
-					transform.position = Vector3.MoveTowards (gameObject.transform.position, WayPoints [WayPointsIndex].transform.position, 4 * Time.deltaTime);
+					StartCoroutine ("WaitforSeconds");
 				}
 			}
 		}
