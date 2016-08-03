00b32bdc815ef0bb4d5ca36ddf466c3f3326669a
Kamzoki <kaittah@ymail.com>
2016-08-03 15:41:52 +0300

    SFX on trigger play added

---
+++ Scripts/OnTriggerSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTriggerSound : MonoBehaviour
+{
+	private AudioSource SFX;
+	public GameObject target;
+	// Use this for initialization
+	void Start ()
+	{
+		SFX = gameObject.GetComponent <AudioSource> ();
+
+		if (target == null) {
+			target = GameObject.FindGameObjectWithTag ("Player");
+		}
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject == target) {
+			SFX.Play ();
+		}
+	}
+}
