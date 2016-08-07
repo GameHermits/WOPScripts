a6ad9cd5df51dc26cc9384d459b9b2d04afe7a14
Kamzoki <kaittah@ymail.com>
2016-08-07 21:14:07 +0300

    done

--- Scripts/RainEffectChaser.cs
+++ Scripts/RainEffectChaser.cs
@@ -7,6 +7,8 @@ public class RainEffectChaser : MonoBehaviour
 
 	// the max distance between the partice and the player that will make the particle reset it's position .
 	public float maxDistance;
+	//Indicating particle X position from the player when it's position is reseted.
+	public float xPosition;
 	// Indicating particle hight from the player when it's position is reseted.
 	public float yPosition;
 	// Indicating partice Z position from the player when it's position is reseted.
@@ -18,24 +20,23 @@ public class RainEffectChaser : MonoBehaviour
 	private float currentDistance;
 	//Indicating the value of adding particle Z position and player Z position to determine weather to move forward or backward
 	private float zValue;
-
+	private ParticleSystem thisParticle;
 	// Use this for initialization
 	void Awake ()
 	{
 		Player = GameObject.FindWithTag ("Player").transform;
+		thisParticle = (ParticleSystem)gameObject.GetComponent ("ParticleSystem");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		currentDistance = Vector3.Distance (transform.position, Player.transform.position);
-		zValue = transform.position.z + Player.transform.position.z;
+
 		if (currentDistance > maxDistance) {
-			if (zValue > 0) {
-				gameObject.transform.position = Player.transform.position + new Vector3 (0, yPosition, -zPosition);	
-			} else {
-				gameObject.transform.position = Player.transform.position + new Vector3 (0, yPosition, zPosition);
-			}
+			thisParticle.Stop ();
+			gameObject.transform.position = Player.transform.position + new Vector3 (xPosition, yPosition, zPosition);
+			thisParticle.Play ();
 		}
 	}
 }
